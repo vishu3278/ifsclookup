31feeedf1f89746037c794d8270b5a74308db179
vishu3278 <vishu3278@gmailcom>
2021-12-09 00:14:02 +0530

    home page

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,94 @@
 <template>
-    <v-app>
-        <v-app-bar app elevate-on-scroll color="white">
-            <div class="d-flex align-center">
-                <v-img alt="Vuetify Name" class="shrink mt-1 " contain min-width="100" src="./assets/logo.png" width="100" />
+    <div>
+        <nav class="navbar is-light is-fixed-top has-shadow" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="/">
+                    <img src="./assets/logo.png" width="66" height="28">
+                </a>
+                <a role="button" class="navbar-burger" aria-label="menu" v-on:click="menuToggle">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
-            <v-spacer></v-spacer>
-            <router-link to="/" exact active-class="red--text" class="btn mx-2">Home</router-link>
-            <router-link to="/about" active-class="red--text" class="btn mx-2">About Us</router-link>
-        </v-app-bar>
-        <v-main>
-            <v-container>
+            <div id="navbarBasicExample" class="navbar-menu" v-bind:class="{'is-active': menuActive}">
+                <div class="navbar-start">
+                    <!-- <a class="navbar-item">
+                    Home
+                </a>
+                <a class="navbar-item">
+                    Documentation
+                </a> -->
+                    <!-- <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        More
+                    </a>
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            About
+                        </a>
+                        <a class="navbar-item">
+                            Jobs
+                        </a>
+                        <a class="navbar-item">
+                            Contact
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Report an issue
+                        </a>
+                    </div>
+                </div> -->
+                </div>
+                <div class="navbar-end">
+                    <!-- <div class="navbar-item"> -->
+                    <router-link to="/" exact active-class="red--text" class="navbar-item">Home</router-link>
+                    <router-link to="/about" active-class="red--text" class="navbar-item">About Us</router-link>
+                    <!-- <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                    </div> -->
+                    <!-- </div> -->
+                </div>
+            </div>
+        </nav>
+        <section class="hero is-small is-info">
+            <div class="hero-body">
+                <p class="title">
+                    IFSC Lookup
+                </p>
+                <p class="subtitle">
+                    Look for IFSC and Bank details
+                </p>
+            </div>
+        </section>
+        <main class="main">
+            <div class="container">
                 <router-view />
-            </v-container>
-        </v-main>
-        <v-footer padless color="primary lighten-2 mt-5">
-            <v-row justify="center" no-gutters>
-                <v-col class="primary lighten-3 py-4 text-center white--text" cols="12">
-                    <v-btn v-for="(link, index) in links" :key="index" :to="link.url" color="white" text class="my-2">
-                        {{ link.text }}
-                    </v-btn>
-                    <v-container>
-                        <p class="text--disabled text-body-2 font-weight-light"><strong>Disclaimer</strong>: We have tried our best to keep the information updated as available from RBI, users are requested to confirm information with the respective bank before using the information provided. The author reserves the right not to be responsible for the topicality, correctness, completeness or quality of the information provided. Liability claims regarding damage caused by the use of any information provided, including any kind of information which is incomplete or incorrect, will therefore be rejected.</p>
-                    </v-container>
-                </v-col>
-                    <v-col class="primary lighten-3 py-4 text-center white--text" cols="12">
-                        &copy; {{ new Date().getFullYear() }} — <strong>IFSC Lookup</strong>
-                    </v-col>
-            </v-row>
-        </v-footer>
-    </v-app>
+            </div>
+        </main>
+        <footer class="footer">
+            <div class="container">
+                <div class="block">
+                    <div class="buttons are-small is-centered ">
+                        <router-link v-for="(link, index) in links" :key="index" :to="link.url" text class="button ">
+                            {{ link.text }}
+                        </router-link>
+                    </div>
+                </div>
+                <div class="block  has-text-justified">
+                    <p><strong>Disclaimer</strong>: We have tried our best to keep the information updated as available from RBI, users are requested to confirm information with the respective bank before using the information provided. The author reserves the right not to be responsible for the topicality, correctness, completeness or quality of the information provided. Liability claims regarding damage caused by the use of any information provided, including any kind of information which is incomplete or incorrect, will therefore be rejected.</p>
+                </div>
+                <p class="has-text-centered has-text-grey-light">
+                    &copy; {{ new Date().getFullYear() }} — <span class="has-text-weight-bold">IFSC Lookup</span>
+                </p>
+            </div>
+        </footer>
+    </div>
 </template>
 <script>
 // import HelloWorld from './components/HelloWorld';
@@ -44,14 +104,21 @@ export default {
     components: {
         // HelloWorld,
     },
-
-    data: () => ({
-        links: [
-            { text: 'Home', url: '/' },
-            { text: 'About Us', url: '/about' },
-            { text: 'Privacy', url: '/privacy' },
-            { text: 'Contact Us', url: '/contact' },
-        ],
-    }),
+    data() {
+        return {
+            links: [
+                { text: 'Home', url: '/' },
+                { text: 'About Us', url: '/about' },
+                { text: 'Privacy', url: '/privacy' },
+                { text: 'Contact Us', url: '/contact' },
+            ],
+            menuActive: false
+        }
+    },
+    methods: {
+        menuToggle() {
+            this.menuActive = !this.menuActive
+        }
+    }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
                             </v-list-item-avatar>
                             <v-list-item-content>
                                 <router-link :to="{name: 'Bank', params: {bankid: bank.id, bankname: bank.name, slug: bank.slug, fquery: filterquery}}" class="grey--text text--darken-3">
-                                    {{bank.name}}<br>{{bank.slug}}
+                                    {{bank.name}}
                                 </router-link>
                             </v-list-item-content>
                         </v-list-item>
@@ -29,19 +29,19 @@
                             </v-list-item-avatar>
                             <v-list-item-content>
                                 <router-link :to="{name: 'Bank', params: {bankid: bank.id, bankname: bank.name, slug: bank.slug, fquery: filterquery}}" class="blue-grey--text text--darken-3">
-                                    {{bank.name}}<br>{{bank.slug}}
+                                    {{bank.name}}
                                 </router-link>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list>
                 </v-col>
                 <v-col>
-                    <v-sheet rounded outlined class="pa-3" color="light-blue lighten-5" style="position:sticky; top:70px;">
+                    <div class="box has-background-info-light p-4" style="position:sticky; top:70px;">
                         <p class="text--secondary"><strong>IFSC</strong> code is allotted by the Reserve Bank of India (RBI) to all banks and its branches. The IFSC code of a bank can be commonly found on a bank account passbook, cheque leaf issued by the bank or on the RBI website. Any kind of fund transfer through a bank requires a valid IFSC. The different types of fund transfers are NEFT, RTGS and IMPS.</p>
                         <h3>How to find IFSC Code?</h3>
                         <p class="text--secondary">IFSC code can be found on cheque leaf and bank passbook of the respective bank. Banks and respective branch list of IFSC codes can be obtained from Reserve Bank of India’s website. The IFSC code of a particular bank can also be found on the banks’ official website.</p>
                         <p class="text--secondary"><strong>MICR</strong> Code (Magnetic Ink Character Recognition) as printed on cheque book to facilitate the processing of cheques.</p>
-                    </v-sheet>
+                    </div>
                 </v-col>
             </v-row>
             <v-overlay :value="loading">
